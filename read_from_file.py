742755793b85443f8f4c30b26441bf3462d41c28
bakefayat <bakefayat98@gmail.com>
2022-06-15 23:34:16 +0430

    initial commit

---
+++ read_from_file.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def add_to_set():
+    pan = pd.read_excel('customers.xlsx')
+    df = pd.DataFrame(pan, )
+    df = df.replace({np.nan: None})
+    customer = df['customer'].tolist()
+    chance = df['chance'].tolist()
+    customers = []
+    chances = []
+    for i in range(0, len(customer)):
+        if chance[i] and customer[i]:
+            customers.append(customer[i])
+            chances.append(chance[i])
+
+    info = zip(customers, chances)
+    return list(info)
